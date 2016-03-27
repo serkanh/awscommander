0e3983c1e8fb8ec7de19912c89797e40d9861d54
Serkan Haytac <shaytac@gmail.com>
2016-03-27 15:25:26 -0400

    aws-sdk added

--- awscommander-ec2.js
+++ awscommander-ec2.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var AWS = require('aws-sdk');
 
+var ec2 = new AWS.EC2({region: 'us-east-1'});
 
 program
   .action(function(cmd){
      switch(cmd){
          case "getInstanceId":            
             console.log("getInstanceId called with "+program.args[1])
+            // create the AWS.Request object
+            var request = ec2.describeInstances(function(error, data) {
+                if (error) {
+                    console.log(error); // an error occurred
+                } else {
+                    console.log(data); // request succeeded
+                }
+            });
+
+            // register a callback to report on the data
+            request.on('success', function(resp) {
+                console.log(resp.data); // log the successful data response
+            });
+
+            // send the request
+            request.send();
             break;
          case "listAllInstances":
             console.log("getInstanceId called")
